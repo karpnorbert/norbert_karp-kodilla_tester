d9e77022fe8e19ce52e3aa52420904c5ab028652
Norbert Karp <norbertkarp2@gmail.com>
2023-03-05 11:16:10 +0100

    Add Class MainExtednded

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        String world = "Hello from FirstClass!";
+        System.out.println(world); // [3]
+
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+    int temperature = 12;
+    String weekday = "Monday";
+    boolean hasBlueEyes = false;
+    char surename = 'N';
+    int height = 185;
+    String month = "March";
+    System.out.println(temperature);
+    System.out.println(weekday);
+    System.out.println(hasBlueEyes);
+    System.out.println(surename);
+    System.out.println(height);
+    }
+}
