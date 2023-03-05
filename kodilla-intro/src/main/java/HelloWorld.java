6da9983ebdc6ae864f9950cacae93bdd0490b1b9
Norbert Karp <norbertkarp2@gmail.com>
2023-03-05 10:53:01 +0100

    Add class HelloWorld

--- kodilla-intro/src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-        System.out.println("Norbert");
-        System.out.println(26);
-        System.out.println('K');
-    }
-}
