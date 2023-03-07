fa0ae3e862bbb3acc07068bb711126112c4f4e24
Norbert Karp <norbertkarp2@gmail.com>
2023-03-07 12:57:12 +0100

    Add class SimpleArray

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
     public static void main(String[] args) {
 
-        int year = 2024;
+        int year = 2023;
 
         if (year % 4 ==0 && year % 100 != 0 || year % 400 == 0){
             System.out.println("Ten rok jest przestępny!");
---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,5 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+    }
+}
