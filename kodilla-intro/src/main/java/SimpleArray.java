f13395b213a0617a419dc8372560020ea58a572c
Norbert Karp <norbertkarp2@gmail.com>
2023-03-07 14:11:22 +0100

    added values for SimpleArray

--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,5 +1,15 @@
 public class SimpleArray {
     public static void main(String[] args) {
+        String[] pets = new String[5];
+        pets[0] = "dog";
+        pets[1] = "cat";
+        pets[2] = "cow";
+        pets[3] = "rhinoceros";
+        pets[4] = "sheep";
+        String pet = pets[3];
+        System.out.println(pet);
 
+        int number0fElements = pets.length;
+        System.out.println("Moja tablica zawiera elementów: " + number0fElements);
     }
 }
