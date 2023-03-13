6d8b1a509e1ca0666819d9934e7d3d7ddcd171a2
Norbert Karp <norbertkarp2@gmail.com>
2023-03-13 19:08:06 +0100

    Add value for Loops

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,2 +1,12 @@
 public class Loops {
+    public static void main(String[] args) {
+       int[] numbers = {2,3,1,3,5};
+
+       int suma = 0;
+
+       for (int i = 0; i <= 4; i++){
+           suma += numbers[i];
+       }
+       System.out.println(suma);
+    }
 }
