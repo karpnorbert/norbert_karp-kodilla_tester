f8fa24ead8663ab6c165f1a98b58c36feda1dbec
Norbert Karp <norbertkarp2@gmail.com>
2023-03-14 19:01:42 +0100

    add price & weight

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600);
-        System.out.println(notebook.weight);
+        Notebook notebook = new Notebook(600, 2000);
+        System.out.println("Notebook weight: " + notebook.weight + " " + "Notebook price: " + notebook.price);
+
+        Notebook oldNotebook = new Notebook(1600, 1000);
+        System.out.println("oldNotebook weight: " + notebook.weight + " " + "oldNotebook price: " + notebook.price);
+
+        Notebook heavyNotebook = new Notebook(2000, 500);
+        System.out.println("heavyNotebook weight: " + notebook.weight + " " + "heavyNotebook price: " + notebook.price);
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,6 +1,18 @@
 public class Notebook {
     int weight;
-    public Notebook(int weight) {
+    int price;
+
+    public Notebook(int weight, int price) {
         this.weight = weight;
+        this.price = price;
+    }
+    public void oldNotebook(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public void heavyNotebook(int weight, int price){
+        this.weight = weight;
+        this.price = price;
     }
 }
