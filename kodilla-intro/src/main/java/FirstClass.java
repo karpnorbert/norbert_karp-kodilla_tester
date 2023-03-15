08e34e4820f04d7a9226b764be3733b2c8a0e2d6
Norbert Karp <norbertkarp2@gmail.com>
2023-03-15 11:58:09 +0100

    Add year

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,23 +2,25 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println(" ");
 
-        Notebook notebook = new Notebook(600, 1000);
-        System.out.println("Notebook weight: " + notebook.weight + " " + "Notebook price: " + notebook.price);
+        Notebook notebook = new Notebook(600, 1000, 2021);
+        System.out.println("Notebook weight: " + notebook.weight + " " + "Notebook price: " + notebook.price + " " + "Notebook year: " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
-
+        notebook.checkYearAndPrice();
         System.out.println(" ");
 
-        Notebook heavyNotebook = new Notebook(2000, 1500);
-        System.out.println("heavyNotebook weight: " + heavyNotebook.weight + " " + "heavyNotebook price: " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2010);
+        System.out.println("heavyNotebook weight: " + heavyNotebook.weight + " " + "heavyNotebook price: " + heavyNotebook.price + " " + "heavyNotebook year: " + heavyNotebook.year);
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
+        heavyNotebook.checkYearAndPrice();
 
         System.out.println(" ");
 
-        Notebook oldNotebook = new Notebook(1600, 500);
-        System.out.println("oldNotebook weight: " + oldNotebook.weight + " " + "oldNotebook price: " + oldNotebook.price);
+        Notebook oldNotebook = new Notebook(1600, 500, 2000);
+        System.out.println("oldNotebook weight: " + oldNotebook.weight + " " + "oldNotebook price: " + oldNotebook.price + " " + "oldNotebook year: " + oldNotebook.year);
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
+        oldNotebook.checkYearAndPrice();
     }
 }
