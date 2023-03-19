dc6582dbe9f35c39e6fc4475b156d4e898ecc927
Norbert Karp <norbertkarp2@gmail.com>
2023-03-19 15:23:07 +0100

    Grades

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -7,11 +7,27 @@ public class Grades {
         this.size = 0;
     }
 
-    public void add(int value) {
+    public void addGrade(int value) {
         if (this.size == 10) {
             return;
         }
         this.grades[this.size] = value;
         this.size++;
+    }
+
+    public int getLastGrade() {
+        if (size > 0) {
+            return grades[size - 1];
+        } else {
+            return 0;
+        }
+    }
+
+    public double getAverage() {
+        double sum = 0;
+        for (int k = 0; k < size; k++) {
+            sum += grades[k];
+        }
+        return sum / size;
     }
 }
