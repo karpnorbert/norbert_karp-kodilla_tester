af4960234eb7f40de7b5193a4ee6eee0f6c7c8e9
Norbert Karp <norbertkarp2@gmail.com>
2023-03-19 14:44:39 +0100

    Add class GradesApp

---
+++ kodilla-intro/src/main/java/GradesApp.java
@@ -0,0 +1,2 @@
+public class GradesApp {
+}
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,2 +1,8 @@
-public class User git{
+public class User {
+    public static void main(String[] args) {
+
+        String[] name = new String[] {"David", "John", "Kate", "Rob", "Carolyne"};
+        int [] age = new int[] {13, 22, 31, 14, 10};
+        System.out.println(name[3] + " " + age[2]);
+    }
 }
