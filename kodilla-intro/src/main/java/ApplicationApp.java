6b4264fd973040990dba824e55497cab7886bce2
Norbert Karp <norbertkarp2@gmail.com>
2023-03-20 15:53:35 +0100

    Add CalcApplication

--- kodilla-intro/src/main/java/ApplicationApp.java
+++ kodilla-intro/src/main/java/ApplicationApp.java
@@ -4,7 +4,7 @@ public class ApplicationApp {
         Application application = new Application("Adam", 40.5, 178);
 
         System.out.println("Name is: " + application.name + " "+ "Age is: " + application.age + " " + "Height is: " + application.height);
+        System.out.println(" ");
         application.checkName();
-
     }
 }
