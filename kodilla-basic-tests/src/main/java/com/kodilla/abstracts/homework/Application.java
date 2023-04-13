a02ba6d181ae521378488389282b0667d3b0dd7a
Norbert Karp <norbertkarp2@gmail.com>
2023-04-13 13:34:56 +0200

    Add package basic_assertion

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Application.java
@@ -7,7 +7,7 @@ public class Application {
         System.out.println("Circuit of Square: " + square.circuit());
 
         Shape rectangle = new Rectangle(20, 30);
-        System.out.println("Area of rectalngle: " + rectangle.area());
+        System.out.println("Area of rectangle: " + rectangle.area());
         System.out.println("Circuit of rectangle: " + rectangle.circuit() );
     }
 }
