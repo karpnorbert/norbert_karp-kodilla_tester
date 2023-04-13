84f17f492ae4cfb3f1f3854229f662340eb66715
Norbert Karp <norbertkarp2@gmail.com>
2023-04-13 14:47:17 +0200

    Add value for CalculatorTestSuite

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,4 +1,13 @@
 package com.kodilla.basic_assertion;
 
 public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
 }
