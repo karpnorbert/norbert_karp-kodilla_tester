58a7367ba5e45090b517522e0b305c363c18408e
Norbert Karp <norbertkarp2@gmail.com>
2023-04-13 13:39:39 +0200

    add class ResultChecker

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,4 +1,11 @@
 package com.kodilla.basic_assertion;
 
 public class Calculator {
+    public int sum(int a, int b){
+        return a + b;
+    }
+    public int subtrack(int a, int b){
+        return a - b;
+    }
 }
+
