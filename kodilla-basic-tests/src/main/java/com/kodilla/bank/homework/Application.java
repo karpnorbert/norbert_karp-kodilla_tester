1d1da4eef1683feb37991787c47e98cc4203f04e
Norbert Karp <norbertkarp2@gmail.com>
2023-04-17 15:55:04 +0200

    Add values for CashMachine

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Application.java
@@ -3,7 +3,7 @@ package com.kodilla.bank.homework;
 public class Application {
     public static void main(String[] args) {
         CashMachine cashMachine = new CashMachine();
-        cashMachine.intiger(55984);
+        cashMachine.init(24425);
         cashMachine.payIn(300);
         cashMachine.payIn(30342);
         cashMachine.payIn(667);
@@ -11,5 +11,10 @@ public class Application {
         cashMachine.payOut(45);
         cashMachine.payOut(665);
         cashMachine.payOut(3659);
+        System.out.println(cashMachine.getSaldo());
+        System.out.println(cashMachine.getAveragePayOut());
+        System.out.println(cashMachine.getAveragePayIn());
+        System.out.println(cashMachine.getNumberOfTransactionsIn());
+        System.out.println(cashMachine.getNumberOfTransactionsOut());
     }
 }
