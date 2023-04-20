aeabbb3e915a97421b7e4f5e3534e426ed413330
Norbert Karp <norbertkarp2@gmail.com>
2023-04-20 14:46:38 +0200

    CarsListApplication add class

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Circle.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Circle.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.interfaces;
 
+import java.util.Objects;
+
 public class Circle implements Shape {
     private  double radius;
     static double PI = 3.1415927;
@@ -13,6 +15,18 @@ public class Circle implements Shape {
 
     public double getPerimeter() {
         return 2 * PI * radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Circle circle)) return false;
+        return Double.compare(circle.radius, radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
     }
 }
 
--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.interfaces;
 
+import java.util.Objects;
+
 public class Triangle implements Shape {
 
     public double width;
@@ -15,6 +17,18 @@ public class Triangle implements Shape {
     @Override
     public double getArea() {
         return width * height / 2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Triangle triangle)) return false;
+        return Double.compare(triangle.width, width) == 0 && Double.compare(triangle.height, height) == 0 && Double.compare(triangle.hypotenuse, hypotenuse) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height, hypotenuse);
     }
 
     @Override
