5aaa1eafe238c02a9b94225e34e74d58620fef42
Norbert Karp <norbertkarp2@gmail.com>
2023-06-10 13:05:56 +0200

    Add class Enginner

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Doctor.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Doctor.java
@@ -1,4 +1,15 @@
 package com.kodilla.abstracts.homework;
 
-public class Doctor {
+class Doctor extends Job {
+    public Doctor(double salary, String responsibilities) {
+        super(salary, responsibilities);
+    }
+
+    @Override
+    public void displayJobDescription() {
+        System.out.println("Job: Doctor");
+        System.out.println("Responsibilities: " + getResponsibilities());
+        System.out.println("Salary: " + getSalary());
+    }
 }
+
