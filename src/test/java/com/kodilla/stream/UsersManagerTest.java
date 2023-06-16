48ffe791c325dc050932f772771b280626d6dc45
Norbert Karp <norbertkarp2@gmail.com>
2023-06-16 18:05:39 +0200

    add implementation

--- src/test/java/com/kodilla/stream/UsersManagerTest.java
+++ src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -2,15 +2,40 @@ package com.kodilla.stream;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UsersManagerTest {
 
     @Test
-    void main() {
+    void testFilterChemistGroupUsernames() {
+        // given
+        List<String> expectedUsernames = List.of("Walter White", "Gale Boetticher");
+
+        // when
+        List<String> chemistUsernames = UsersManager.filterChemistGroupUsernames();
+
+        // then
+        assertEquals(expectedUsernames, chemistUsernames);
     }
 
     @Test
-    void getUserName() {
+    void testFilterUsersByAge() {
+        // given
+        List<User> users = UsersRepository.getUsersList();
+        int ageThreshold = 40;
+        List<User> expectedUsers = List.of(
+                new User("Walter White", 50, 7, "Chemists"),
+                new User("Gus Firing", 49, 0, "Board"),
+                new User("Gale Boetticher", 44, 2, "Chemists"),
+                new User("Mike Ehrmantraut", 57, 0, "Security")
+        );
+
+        // when
+        List<User> filteredUsers = UsersManager.filterUsersByAge(users, ageThreshold);
+
+        // then
+        assertEquals(expectedUsers, filteredUsers);
     }
 }
