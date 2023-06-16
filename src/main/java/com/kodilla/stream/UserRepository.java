800882c1aa2a98fc1b92cdd6e4f2ad655c95933c
Norbert Karp <norbertkarp2@gmail.com>
2023-06-16 18:02:26 +0200

    Add test for UserManager

--- src/main/java/com/kodilla/stream/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.stream;
-
-public class User {
-    private String username;
-    private int age;
-    private int numberOfPost;
-    private String group;
-}
---
+++ src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersManagerTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void getUserName() {
+    }
+}
