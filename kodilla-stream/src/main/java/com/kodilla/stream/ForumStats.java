36ad0e82170bea2e10850fdc84987b52ee5ec617
Norbert Karp <norbertkarp2@gmail.com>
2023-06-21 15:59:01 +0200

    Add ForumStats and Tests

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/ForumStats.java
@@ -0,0 +1,31 @@
+package com.kodilla.stream.homework;
+
+import java.util.List;
+
+public class ForumStats {
+    public static double calculateAveragePostsForUsersAboveAge(List<User> users, int age) {
+        return users.stream()
+                .filter(user -> user.getAge() >= age)
+                .mapToInt(User::getNumberOfPosts)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static double calculateAveragePostsForUsersBelowAge(List<User> users, int age) {
+        return users.stream()
+                .filter(user -> user.getAge() < age)
+                .mapToInt(User::getNumberOfPosts)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static void main(String[] args) {
+        List<User> users = UsersRepository.getUsers();
+
+        double averagePostsAbove40 = calculateAveragePostsForUsersAboveAge(users, 40);
+        double averagePostsBelow40 = calculateAveragePostsForUsersBelowAge(users, 40);
+
+        System.out.println("Average posts for users above 40: " + averagePostsAbove40);
+        System.out.println("Average posts for users below 40: " + averagePostsBelow40);
+    }
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/ForumStatsTest.java
@@ -0,0 +1,20 @@
+package com.kodilla.stream;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForumStatsTest {
+
+    @Test
+    void calculateAveragePostsForUsersAboveAge() {
+    }
+
+    @Test
+    void calculateAveragePostsForUsersBelowAge() {
+    }
+
+    @Test
+    void main() {
+    }
+}
