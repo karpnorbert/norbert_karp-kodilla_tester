71df0378b646f877e7d5bb0d6bf45798855c96f0
karpnorbert <norbertkarp2@gmail.com>
2023-08-18 20:05:30 +0200

    Edit Forum Stats and add tests for forumStats

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -6,31 +6,25 @@ import com.kodilla.stream.UsersRepository;
 import java.util.List;
 
 public class ForumStats {
-    public static double calculateAveragePostsForUsersAboveAge(List<User> users, int age) {
+
+    public static double avgNumberOfPostsForUsersAbove40(List<User> users) {
         return users.stream()
-                .filter(user -> user.getAge() >= age)
-                .mapToInt(user->user.getNumberOfPosts())
+                .filter(u -> u.getAge() >= 40)
+                .mapToInt(User::getNumberOfPosts)
                 .average()
                 .orElse(0.0);
     }
 
-    public static double calculateAveragePostsForUsersBelowAge(List<User> users, int age) {
+    public static double avgNumberOfPostsForUsersBelow40(List<User> users) {
         return users.stream()
-                .filter(user -> user.getAge() < age)
+                .filter(u -> u.getAge() < 40)
                 .mapToInt(User::getNumberOfPosts)
                 .average()
                 .orElse(0.0);
     }
 
     public static void main(String[] args) {
-        List<User> users = UsersRepository.getUsersList();
-
-        double averagePostsAbove40 = calculateAveragePostsForUsersAboveAge(users, 40);
-        double averagePostsBelow40 = calculateAveragePostsForUsersBelowAge(users, 40);
-
-        System.out.println("Average posts for users above 40: " + averagePostsAbove40);
-        System.out.println("Average posts for users below 40: " + averagePostsBelow40);
+        System.out.println("Średnia liczba postów dla użytkowników >= 40 lat: " + avgNumberOfPostsForUsersAbove40(UsersRepository.getUsersList()));
+        System.out.println("Średnia liczba postów dla użytkowników < 40 lat: " + avgNumberOfPostsForUsersBelow40(UsersRepository.getUsersList()));
     }
 }
-
-
