1aead03b8c7488e0cff184c952b347543a89a231
karpnorbert <norbertkarp2@gmail.com>
2023-08-26 15:14:20 +0200

    add class Warehouse

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Order.java
@@ -1,4 +1,15 @@
 package com.kodilla.exception.homework;
 
 public class Order {
+    public class Order {
+        private String number;
+
+        public Order(String number) {
+            this.number = number;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+    }
 }
