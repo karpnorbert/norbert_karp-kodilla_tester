b70d819de9dd80f1393f55c8a8e9f070dce8df2a
karpnorbert <norbertkarp2@gmail.com>
2023-08-26 15:20:33 +0200

    add tests for AirportRepository

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/AirportRepositoryTestSuite.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirportRepositoryTestSuite {
+
+    @Test
+    void isAirportInUse() {
+    }
+}
