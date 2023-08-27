c6c395dd1371c47a45b575780e1ebd108239a791
karpnorbert <norbertkarp2@gmail.com>
2023-08-27 20:42:15 +0200

    add tests for homework

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidatorTestSuite {
+
+    @Test
+    void validateUsername() {
+    }
+
+    @Test
+    void validateEmail() {
+    }
+}
