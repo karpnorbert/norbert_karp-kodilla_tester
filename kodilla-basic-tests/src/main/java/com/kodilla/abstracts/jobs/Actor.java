7f890263bdd3390aa9b483c5ea496167ccd56d41
karpnorbert <norbertkarp2@gmail.com>
2023-10-10 12:08:19 +0200

    Add class homework and class order & Shop

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/jobs/Actor.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.abstracts.jobs;
-
-public class Actor extends Job {
-
-    public Actor() {
-        super(400, "Singing");
-    }
-}
--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.collections.arrays.homework;
-
-public class CarUtils {
-}
