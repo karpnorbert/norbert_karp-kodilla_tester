7629d6a7ffccb94f4887b885cdea7f7fefcfe9b2
karpnorbert <norbertkarp2@gmail.com>
2023-10-12 13:36:48 +0200

    homework

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/ShippingCenter.java
@@ -0,0 +1,20 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class ShippingCenter {
+
+    private final Delivery delivery;
+    private final Notification notification;
+
+    public ShippingCenter(Delivery delivery, Notification notification) {
+        this.delivery = delivery;
+        this.notification = notification;
+    }
+
+    public void sendPackage(String address, double weight) {
+        if (delivery.deliverPackage(address, weight)) {
+            notification.success(address);
+        } else {
+            notification.fail(address);
+        }
+    }
+}
