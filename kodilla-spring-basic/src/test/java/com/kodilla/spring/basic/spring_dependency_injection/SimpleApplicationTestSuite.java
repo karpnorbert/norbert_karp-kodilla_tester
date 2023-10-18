9d003d41b0e7dcd4cba25b703572b9adbd1cebdf
karpnorbert <norbertkarp2@gmail.com>
2023-10-18 13:25:50 +0200

    homework

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplicationTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplicationTestSuite.java
@@ -1,10 +1,14 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 public class SimpleApplicationTestSuite {
+
     @Test
     public void shouldReturnCorrectMessage() {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic"); // [1]
@@ -13,4 +17,11 @@ public class SimpleApplicationTestSuite {
         Assertions.assertNotNull(message);
     }
 
+    @Test
+    public void shouldProcessMessage() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        SimpleApplication bean = context.getBean(SimpleApplication.class);
+        String message = bean.processMessage("Test", "Any receiver");
+        Assertions.assertEquals("Sending [Test] to: Any receiver using Skype", message);
+    }
 }
