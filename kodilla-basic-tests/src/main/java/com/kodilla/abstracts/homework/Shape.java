ea9aa4c02984b6fb4d658aa05ff093d9ff3bb1ba
karpnorbert <norbertkarp2@gmail.com>
2023-10-18 15:06:02 +0200

    add value for zadanie 10

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.abstracts.homework;
-
-abstract class Shape {
-    public abstract double calculateArea();
-
-    public abstract double calculatePerimeter();
-}
--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/TextMessageService.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.spring.basic.dependency_injection;
-
-public class TextMessageService implements MessageService {
-    @Override
-    public String send(String message, String receiver) {
-        System.out.println("Sending text [" + message + "] to: " + receiver);
-        return message;
-    }
-}
