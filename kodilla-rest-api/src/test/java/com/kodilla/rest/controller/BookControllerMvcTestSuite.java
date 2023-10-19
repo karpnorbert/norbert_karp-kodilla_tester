7efd468ea1f5a0a184bfac3b0a206b36ddb9162e
karpnorbert <norbertkarp2@gmail.com>
2023-10-19 10:20:15 +0200

    add tests for bookControllerMvc & add homework - addBook

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.rest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(BookController.class)
+class BookControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @BeforeEach
+    void setUp() {
+        // Przygotuj mocka przed każdym testem, jeśli to konieczne
+    }
+
+    @Test
+    void shouldAddBook() throws Exception {
+        //given
+        BookDto bookToAdd = new BookDto("Title 1", "Author 1");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonContent = objectMapper.writeValueAsString(bookToAdd);
+
+        //when & then
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+
+        Mockito.verify(bookService, Mockito.times(1)).addBook(bookToAdd);
+    }
+}
