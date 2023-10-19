b4fe35e3e2117ceadf119c529efdda955dd9ceb8
karpnorbert <norbertkarp2@gmail.com>
2023-10-19 10:02:06 +0200

    add delete method for postman

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/Dog.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/Dog.java
@@ -1,8 +1,11 @@
 package com.kodilla.spring.basic.spring_configuration;
 
+import org.springframework.context.annotation.Primary;
+
 public class Dog implements Animal {
 
     @Override
+    @Primary
     public String getVoice() {
         return "Bark bark";
     }
