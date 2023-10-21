44b06b3a328a687f50fc2fe9461a9b2abf8395bd
Norbert Karp <norbertkarp2@gmail.com>
2023-10-21 15:05:17 +0200

    homework for Selenium Ebay

--- kodilla-selenium/src/main/java/com/kodilla/selenium/search/StoreSearchExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/search/StoreSearchExample.java
@@ -1,12 +1,18 @@
 package com.kodilla.selenium.search;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class StoreSearchExample {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/norbertkarp/Documents/Development/Kodilla/chromedriver");  	// [1]
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "/Users/norbertkarp/Documents/Development/Kodilla/geckodriver");  	// [1]
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://kodilla.com/pl/test/store");
+
+        WebElement inputField = driver.findElement(By.name("search"));
+        ((WebElement) inputField).sendKeys("School");
+        inputField.submit();                                  	// [1]
     }
 }
